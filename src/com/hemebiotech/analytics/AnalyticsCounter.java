1ae10c157eb75b9aa52e67a7fbb689ca87b925bc
mark-alexandre <mark.alexandre.strasbourg@gmail.com>
2020-11-06 23:26:20 +0100

    A constant for the resources folder has been added + broken code fixed for the file reader. At this stage, the main method is perfectly reading our symtoms.txt file.

--- src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -1,17 +1,23 @@
 package com.hemebiotech.analytics;
 
+import com.hemebiotech.utils.Constants;
+
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileReader;
 import java.io.FileWriter;
 
 public class AnalyticsCounter {
-	private static int headacheCount = 0;	// initialize to 0
-	private static int rashCount = 0;		// initialize to 0
-	private static int pupilCount = 0;		// initialize to 0
+	private static String resourcesPath = Constants.PROJECT_PATH.getName() + "resources" + File.separator;
+	private static int headacheCount = 0;
+	private static int rashCount = 0;
+	private static int pupilCount = 0;
 	
 	public static void main(String args[]) throws Exception {
-		// first get input
-		BufferedReader reader = new BufferedReader (new FileReader("symptoms.txt"));
+		// Now, we will fetch the file
+		BufferedReader reader = new BufferedReader (
+				new FileReader(resourcesPath + "symptoms.txt")
+		);
 		String line = reader.readLine();
 
 		int i = 0;	// set i to 0
@@ -34,7 +40,7 @@ public class AnalyticsCounter {
 		}
 		
 		// next generate output
-		FileWriter writer = new FileWriter ("result.out");
+		FileWriter writer = new FileWriter (resourcesPath + "result.out");
 		writer.write("headache: " + headacheCount + "\n");
 		writer.write("rash: " + rashCount + "\n");
 		writer.write("dialated pupils: " + pupilCount + "\n");
---
+++ src/com/hemebiotech/utils/Constants.java
@@ -0,0 +1,20 @@
+package com.hemebiotech.utils;
+
+import java.io.File;
+
+public enum Constants {
+
+    PROJECT_PATH(System.getProperty("user.dir") + File.separator);
+
+    private final String name;
+
+    private Constants(String name) {
+        this.name = name;
+    }
+
+    public String getName()
+    {
+        return name;
+    }
+
+}
